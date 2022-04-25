342e222df0951c3dd96709864f42bacf3a10fc2a
gogi999 <g.petrovic9@gmail.com>
2022-04-25 13:19:07 +0200

    Performance techniques and optimization

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { increaseCount, getCount } from '../features/posts/postsSlice';
 
 const Header = () => {
+    const dispatch = useDispatch();
+    const count = useSelector(getCount);
+
     return (
         <header className="Header">
             <h1>Gogi's Blog</h1>
@@ -13,7 +18,17 @@ const Header = () => {
                     <li>
                         <Link to="post">Post</Link>
                     </li>
+                    <li>
+                        <Link to="user">Users</Link>
+                    </li>
                 </ul>
+                <button
+                    onClick={() => 
+                        dispatch(increaseCount())
+                    }
+                >
+                    {count}
+                </button>
             </nav>
         </header>
     );
--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { selectAllPosts, getPostsStatus, getPostsError } from './postsSlice';
+import { selectPostIds, getPostsStatus, getPostsError } from './postsSlice';
 import PostsExcerpt from './PostsExcerpt';
 
 const PostsList = () => {
-    const posts = useSelector(selectAllPosts);
-    const postsStatus = useSelector(getPostsStatus);
+    const orderedPostIds = useSelector(selectPostIds)
+    const postStatus = useSelector(getPostsStatus);
     const error = useSelector(getPostsError);
 
     let content;
-    if (postsStatus === 'loading') {
+    if (postStatus === 'loading') {
         content = <p>"Loading..."</p>;
-    } else if (postsStatus === 'succeeded') {
-        const orderedPosts = posts.slice().sort(
-            (a, b) => b.date.localeCompare(a.date)
-        );
-        content = orderedPosts.map((post) => 
-            <PostsExcerpt  key={post.id} post={post} />);
-    } else if (postsStatus === 'failed') {
-        content = <p>{error}</p>
+    } else if (postStatus === 'succeeded') {
+        content = orderedPostIds.map(postId => (
+            <PostsExcerpt key={postId} postId={postId} />
+        ));
+    } else if (postStatus === 'failed') {
+        content = <p>{error}</p>;
     }
 
     return (
